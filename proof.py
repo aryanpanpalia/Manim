69e0ebd980d66b65679af9f17081329bc972664f
Aryan Panpalia <aryanpanpalia@gmail.com>
2021-06-03 23:06:05 -0400

    Improve permute

--- proof.py
+++ proof.py
@@ -63,27 +63,39 @@ class CustomArrowTip(ArrowTip, Triangle, ABC):
 
 
 class Permutation(Scene):
-    def permute(self, dots):
-        permutations = [
-            [0, 4, 10, 17, 7, 0],
-            [1, 9, 16, 1],
-            [2, 6, 14, 5, 19, 2],
-            [3, 11, 15, 18, 3],
-            [8, 12, 13, 8],
-
-        ]
+    def permute(self, dots, labels, permutations):
         transforms = []
-        fade_in_arrows = []
+        create_arrows = []
+        destroy_arrows = []
+        fade_out_labels = [FadeOut(label) for label in labels]
 
         for permutation in permutations:
             for index in range(len(permutation) - 1):
-                transforms.append(Transform(dots[permutation[index]], dots[permutation[index + 1]]))
-                arrow = Arrow(dots[permutation[index]].get_center(), dots[permutation[index + 1]].get_center(),
-                              stroke_width=2, tip_shape=CustomArrowTip, buff=0.15)
-                fade_in_arrows.append(FadeIn(arrow))
+                transforms.append(
+                    Transform(
+                        dots[permutation[index]], dots[permutation[index + 1]]
+                    )
+                )
+                arrow = Arrow(
+                    dots[permutation[index]].get_center(),
+                    dots[permutation[index + 1]].get_center(),
+                    stroke_width=2,
+                    tip_shape=CustomArrowTip,
+                    buff=0.15
+                )
+                create_arrows.append(Create(arrow))
+                destroy_arrows.append(Uncreate(arrow))
 
-        self.play(*fade_in_arrows)
+        self.play(*create_arrows)
+        self.play(*fade_out_labels)
         self.play(*transforms, run_time=3)
+        self.play(*destroy_arrows)
+
+        fade_in_labels = [FadeIn(label) for label in labels]
+        for label, dot in zip(labels, dots):
+            label.move_to(dot.get_center()*1.1)
+
+        self.play(*fade_in_labels)
 
     def construct(self):
         circle = Circle(radius=3, color=BLACK)
@@ -93,18 +105,28 @@ class Permutation(Scene):
 
         points = []
         dots = []
+        labels = []
 
         for c, angle in enumerate(np.linspace(0, TAU, num_points, endpoint=False)):
-            p = circle.point_at_angle(angle)
-            points.append(p)
-            d = Dot(point=p)
-            dots.append(d)
-            label = Integer(number=c + 1).move_to(p * 1.1).scale(0.5)
-            self.add(d, label)
+            point = circle.point_at_angle(angle)
+            points.append(point)
+            dot = Dot(point=point)
+            dots.append(dot)
+            label = Integer(number=c + 1).scale(0.5).move_to(dot.get_center() * 1.1)
+            labels.append(label)
+            self.add(dot, label)
 
-        self.permute(dots)
+        permutations = [
+            [0, 4, 10, 17, 7, 0],
+            [1, 9, 16, 1],
+            [2, 6, 14, 5, 19, 2],
+            [3, 11, 15, 18, 3],
+            [8, 12, 13, 8],
+        ]
+
+        self.permute(dots, labels, permutations)
         self.wait()
-        self.permute(dots)
+        self.permute(dots, labels, permutations)
         self.wait()
-        self.permute(dots)
+        self.permute(dots, labels, permutations)
         self.wait()
