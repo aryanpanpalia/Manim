41d502c483e8a0306e65ee3c0ef634e702d127a8
Aryan Panpalia <aryanpanpalia@gmail.com>
2021-08-10 00:06:21 -0400

    Start IntroduceProblem scene
    broken but sorta the beginning of something

--- Video 2/proof.py
+++ Video 2/proof.py
@@ -1,3 +1,4 @@
+from typing import Awaitable
 from manim import *
 
 
@@ -14,32 +15,67 @@ def integrate(f, a, b):
     return area
 
 
-class Learning(Scene):
+class IntoduceProblem(Scene):
 
     @staticmethod
     def integral(f, lambd):
-        return integrate(lambda x: f(x) * np.sin(lambd * x), 0, 1)
+        return integrate(lambda x: f(x) * np.cos(lambd * x), 0, 1)
 
     def construct(self):
         axes = Axes(
-            x_range=[0, 250, 50],
-            y_range=[-5, 5, 5],
-            x_axis_config={"numbers_to_include": np.arange(0, 251, 50)},
-            y_axis_config={"numbers_to_include": [-5, 0, 5]},
+            x_range=[-0.1, 1.1, 1],
+            y_range=[-10, 10, 5],
+            x_axis_config={"numbers_to_include": np.array([0, 1])},
+            y_axis_config={"numbers_to_include": [-10, -5, 0, 5, 10]},
+            x_length=10,
             y_length=5,
-            tips=False
-        )
+            tips=True
+        ).shift(DOWN * 1)
         labels = axes.get_axis_labels(
-            x_label=MathTex("\lambda"), y_label=MathTex("\int_0^1 20x^2 \sin(\lambda x) \\text{d}x")
+            x_label=MathTex("x"), y_label=MathTex("y")
         )
-        print(axes.x_axis_config)
 
         graph = axes.get_graph(
-            lambda lambd: self.integral(lambda x: 20 * x ** 2, lambd),
-            x_range=[0, 250],
-            color=YELLOW
+            lambda x: 120 * (x - 0.5) ** 3 - 9 * x ** 2 + 5,
+            x_range=[0, 1],
+            color
+            =YELLOW
         )
 
+        equation = MathTex("f(x)", ".").shift(UP * 3)
+
         self.play(Create(axes), Write(labels))
-        self.play(Create(graph))
+        self.play(Create(graph), Write(equation))
+
+        lambd = ValueTracker(25)
+        new_graph = axes.get_graph(
+            lambda x: (120 * (x - 0.5) ** 3 - 9 * x ** 2 + 5) * np.cos(lambd.get_value() * x),
+            x_range=[0, 1],
+            color=YELLOW
+        )
+        new_equation = MathTex("f(x)", "\cos(\lambda x).").shift(UP * 3)
+        lambda_onscreen = MathTex("\lambda = ", f"{lambd.get_value()}").shift(UP * 3 + RIGHT * 5)
+
+        self.play(Transform(graph, new_graph), Transform(equation[0], new_equation[0]), Transform(equation[1], new_equation[1]), FadeIn(lambda_onscreen))
         self.wait()
+        graph = new_graph
+        area = axes.get_area(graph, [0, 1], color=BLUE, opacity=0.5, dx_scaling=0.5)
+        self.play(FadeIn(area))
+        self.wait()
+        # def update_for_lambda():
+        #     graph = axes.get_graph(
+        #         lambda x: (120 * (x - 0.5) ** 3 - 9 * x ** 2 + 5) * np.cos(lambd.get_value() * x),
+        #         x_range=[0, 1],
+        #         color=YELLOW
+        #     )
+        #     area = axes.get_area(graph, [0, 1], color=BLUE, opacity=0.5)
+        #     lambda_onscreen = MathTex("\lambda = ", f"{round(lambd.get_value(), 2)}").shift(UP * 3 + RIGHT * 5)
+        #     return VGroup(graph, area, lambda_onscreen)
+
+        # update = always_redraw(update_for_lambda)
+
+        # self.remove(graph, lambda_onscreen, area)
+        # self.add(update)
+
+        # self.play(lambd.animate(rate_func=rate_functions.linear).set_value(50), run_time=1)
+        # self.wait()
