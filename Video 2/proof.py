db0f960c23ace5f8dece753aee77088ac1f08901
Aryan Panpalia <aryanpanpalia@gmail.com>
2021-08-10 22:45:27 -0400

    Fixed IntroduceProblem scene

--- Video 2/proof.py
+++ Video 2/proof.py
@@ -37,45 +37,53 @@ class IntoduceProblem(Scene):
 
         graph = axes.get_graph(
             lambda x: 120 * (x - 0.5) ** 3 - 9 * x ** 2 + 5,
-            x_range=[0, 1],
+            x_range=[0, 1, 1e-3],
             color
             =YELLOW
         )
 
-        equation = MathTex("f(x)", ".").shift(UP * 3)
+        equation = MathTex("f(x)").shift(UP * 3)
 
         self.play(Create(axes), Write(labels))
         self.play(Create(graph), Write(equation))
 
-        lambd = ValueTracker(25)
+        lambd = ValueTracker(0)
         new_graph = axes.get_graph(
             lambda x: (120 * (x - 0.5) ** 3 - 9 * x ** 2 + 5) * np.cos(lambd.get_value() * x),
-            x_range=[0, 1],
+            x_range=[0, 1, 1e-3],
             color=YELLOW
         )
-        new_equation = MathTex("f(x)", "\cos(\lambda x).").shift(UP * 3)
+        new_equation = MathTex("f(x) \cos(\lambda x)").shift(UP * 3)
         lambda_onscreen = MathTex("\lambda = ", f"{lambd.get_value()}").shift(UP * 3 + RIGHT * 5)
 
-        self.play(Transform(graph, new_graph), Transform(equation[0], new_equation[0]), Transform(equation[1], new_equation[1]), FadeIn(lambda_onscreen))
+        self.play(Transform(graph, new_graph), Transform(equation, new_equation), FadeIn(lambda_onscreen))
         self.wait()
-        graph = new_graph
-        area = axes.get_area(graph, [0, 1], color=BLUE, opacity=0.5, dx_scaling=0.5)
-        self.play(FadeIn(area))
+        
+        new_equation = MathTex(r"\int_0^1 f(x) \cos(\lambda x) \text{d}x = ").shift(UP * 3 + LEFT)
+        integral_value_text = MathTex(
+            f"{round(integrate(lambda x: (120 * (x - 0.5) ** 3 - 9 * x ** 2 + 5) * np.cos(lambd.get_value() * x), 0, 1), 2)}"
+        ).next_to(new_equation, RIGHT)
+
+        area = axes.get_area(graph, [0, 1], color=BLUE, opacity=0.5, dx_scaling=0.1)
+        self.play(FadeIn(area), Transform(equation, new_equation), FadeIn(integral_value_text))
+
+        def update_for_lambda():
+            graph = axes.get_graph(
+                lambda x: (120 * (x - 0.5) ** 3 - 9 * x ** 2 + 5) * np.cos(lambd.get_value() * x),
+                x_range=[0, 1, 1e-3],
+                color=YELLOW
+            )
+            area = axes.get_area(graph, [0, 1], color=BLUE, opacity=0.5)
+            lambda_onscreen = MathTex("\lambda = ", f"{round(lambd.get_value(), 2)}").shift(UP * 3 + RIGHT * 5)
+            integral_value_text = MathTex(
+                f"{round(integrate(lambda x: (120 * (x - 0.5) ** 3 - 9 * x ** 2 + 5) * np.cos(lambd.get_value() * x), 0, 1), 2)}"
+            ).next_to(new_equation, RIGHT)
+            return VGroup(graph, area, lambda_onscreen, integral_value_text)
+
+        update = always_redraw(update_for_lambda)
+
+        self.remove(graph, lambda_onscreen, area, new_graph, integral_value_text)
+        self.add(update)
+
+        self.play(lambd.animate(rate_func=rate_functions.linear).set_value(50), run_time=1)
         self.wait()
-        # def update_for_lambda():
-        #     graph = axes.get_graph(
-        #         lambda x: (120 * (x - 0.5) ** 3 - 9 * x ** 2 + 5) * np.cos(lambd.get_value() * x),
-        #         x_range=[0, 1],
-        #         color=YELLOW
-        #     )
-        #     area = axes.get_area(graph, [0, 1], color=BLUE, opacity=0.5)
-        #     lambda_onscreen = MathTex("\lambda = ", f"{round(lambd.get_value(), 2)}").shift(UP * 3 + RIGHT * 5)
-        #     return VGroup(graph, area, lambda_onscreen)
-
-        # update = always_redraw(update_for_lambda)
-
-        # self.remove(graph, lambda_onscreen, area)
-        # self.add(update)
-
-        # self.play(lambd.animate(rate_func=rate_functions.linear).set_value(50), run_time=1)
-        # self.wait()
